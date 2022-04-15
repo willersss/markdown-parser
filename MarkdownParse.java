88f764c8cc68d18c10ab382156ad663bdf32a591
willersss <103155875+willersss@users.noreply.github.com>
2022-04-15 13:18:56 -0700

    Fixed Empty Line Error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,8 +16,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if (openBracket != -1) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else {
+                break;
+            }
         }
 
         return toReturn;
