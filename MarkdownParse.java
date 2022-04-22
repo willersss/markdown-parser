9ec91132e14c6ce7e977e2de0e85c3b975c855d0
willersss <103155875+willersss@users.noreply.github.com>
2022-04-22 12:48:59 -0700

    error changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,20 +9,37 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
+        // Find the next [, then find the ], then get the first starting
+        // character of the link. Proceed to find the last character by finding the new
+        // line (if there is no new line that means you are at the end of the file so get
+        // the whole length and subtract)
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket != -1) {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
+
+            int beginLink = closeBracket + 2; // Skip to the start of the link
+
+            int newLine = markdown.indexOf("\n", closeBracket); // Find a new line if there is one
+
+            // If there is no newline then just get end of whole file and subtract
+            int endLink;
+            if (newLine == -1) {
+                newLine = markdown.length(); 
+                endLink = newLine - 1; // Get index of end of link
+            } else {
+                endLink = newLine - 2; // Get index of end of link
             }
-            else {
+
+            String link;
+            if (openBracket != -1) {
+                link = markdown.substring(beginLink, endLink);
+                toReturn.add(link);
+                currentIndex = newLine;
+            } else {
                 break;
             }
+            System.out.println(link);
         }
 
         return toReturn;
---
+++ new-test2.md
@@ -0,0 +1,2 @@
+# Title
+link1 [link2][some-thing.html]
